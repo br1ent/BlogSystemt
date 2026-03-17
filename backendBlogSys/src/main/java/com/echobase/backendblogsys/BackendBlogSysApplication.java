80b529b2cf9f272b9d1cc5159e952076bdc05e98
石铭轩 <530220399@qq.com>
2026-03-17 20:40:55 +0800

    init project

---
+++ backendBlogSys/src/main/java/com/echobase/backendblogsys/BackendBlogSysApplication.java
@@ -0,0 +1,13 @@
+package com.echobase.backendblogsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBlogSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBlogSysApplication.class, args);
+    }
+
+}
---
+++ backendBlogSys/src/test/java/com/echobase/backendblogsys/BackendBlogSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echobase.backendblogsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBlogSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
